6d35650fa931e059748af03078d2333e6ff8deb7
Sergey Gusarov <s3rg.rov@yandex.ru>
2021-12-22 20:01:21 +0300

    refactoring tag cloud

--- src/main/java/com/example/MyBookShopApp/model/tag/Tag.java
+++ src/main/java/com/example/MyBookShopApp/model/tag/Tag.java
@@ -18,6 +18,13 @@ public class Tag {
     @Formula("(SELECT COUNT(*) FROM book2tag b WHERE b.tag_id=id)")
     private Integer count;
 
+    public String font() {
+        if (count >= 0 && count < 5) return "Tag_xs";
+        if (count >= 5 && count < 10) return "Tag_sm";
+        if (count >= 10 && count < 15) return "Tag_md";
+        return "Tag_lg";
+    }
+
     public Integer getId() {
         return id;
     }
